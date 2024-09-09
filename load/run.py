d90b8e1958002931984badd98444165d90c85cef
jiyoung-B <catgirl0313@naver.com>
2024-09-09 21:42:56 +0900

    extract from csv

---
+++ load/run.py
@@ -0,0 +1,4 @@
+# import psycopg2
+
+# install psycop2, uncomment the first line and implement your code below
+
---
+++ transform/table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE person (
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR(128),
+	last_name VARCHAR(128),
+	score INTEGER);
