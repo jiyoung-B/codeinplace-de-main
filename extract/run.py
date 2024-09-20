2db7ac434d96f46ea12d8ef6f2775c993dad7a24
jiyoung-B <catgirl0313@naver.com>
2024-09-20 21:00:24 +0900

    extract 리팩토링

--- extract/run.py
+++ extract/run.py
@@ -1,11 +1,36 @@
 # implement your code here
 import csv
 
-file_path = "C:\\Users\\User\PycharmProjects\codeinplace-de-main\extract\data.csv"
+def extract_data():
+    file_path = "C:\\Users\\User\PycharmProjects\codeinplace-de-main\extract\data.csv"
 
-data = open(file_path)
+    data = open(file_path)
 
-reader = csv.reader(data)
-lines = list(reader)
+    reader = csv.reader(data)
+    lines = list(reader)
+    print("-------------------------------")
+    print(lines)
+    print("-------------------------------")
+    datalist = []
 
-print(lines)
+    for row in lines:
+        name = row[0].strip().lower()
+        number = int(row[1].strip())
+        datalist.append([name, number])
+    print(datalist)
+    data.close()
+    return datalist
+
+
+if __name__ == "__main__":
+    extract_data()
+
+
+
+
+
+# assert result == [
+#     ["lionel messi", 97],
+#     ["jisung park", 99],
+#     ["heungmin son", 102],
+# ]
