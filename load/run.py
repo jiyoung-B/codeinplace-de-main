88b74892b7e33ff9854ca0f7a5a7688bcebc5e2c
jiyoung-B <catgirl0313@naver.com>
2024-09-21 20:50:43 +0900

    load - 함수 생성 db 커넥션 연결 및 닫기 insert select

--- load/run.py
+++ load/run.py
@@ -1,5 +1,6 @@
 # import psycopg2
 import psycopg2
+'''
 
 # install psycop2, uncomment the first line and implement your code below
 db = psycopg2.connect(host='localhost', dbname='de_test',user='postgres',password='1234',port=5432)
@@ -24,8 +25,7 @@ print(f"Selected Row: {row}")
 cursor.close()
 db.close()
 
-
-# '''
+'''
 # create a database called de_test in postgresql # postresql 에서 진행
 # create a table defined in table.sql # postresql 에서 진행
 '''
@@ -42,3 +42,83 @@ db.close()
 # select inserted row using psycopg2
 # run command: python3 run.py
 # '''
+
+
+
+# db 서버 연결 및 커서 생성
+def connect_db():
+    try:
+        db = psycopg2.connect(host='localhost', dbname='de_test',user='postgres',password='1234',port=5432)
+        cursor = db.cursor()
+        return db, cursor
+    except Exception as e:
+        print(e)
+        print("DB 연결 실패")
+        return None
+
+# 커서 및 db 종료
+def close_db(db, cursor):
+    if cursor:
+        cursor.close()
+    if db:
+        db.close()
+
+# insert
+def insert(db, cursor, first_name, last_name, score):
+    try:
+        cursor.execute(
+            "INSERT INTO person (first_name, last_name, score) VALUES (%s, %s, %s) RETURNING id;",
+            (first_name, last_name, score)
+        )
+        inserted_id = cursor.fetchone()[0]
+        db.commit()  # 변경 사항을 DB에 반영
+        print(f"삽입 ID: {inserted_id}")
+        return inserted_id
+    except Exception as e:
+        print(f"삽입 실패: {e}")
+        db.rollback()  # 실패 시 롤백
+        return None
+
+
+# select
+def select(db, cursor, person_id):
+    try:
+        cursor.execute("SELECT * FROM person WHERE id = %s;", (person_id,))
+        row = cursor.fetchone()
+        print(f"검색: {row}")
+        return row
+    except Exception as e:
+        print(e)
+        db.rollback()
+        return None
+
+def select_n(db, cursor, n):
+    try:
+        #cursor.execute("SELECT * FROM person;")
+        #cursor.execute("SELECT first_name, last_name, score FROM person;")
+        cursor.execute("SELECT first_name, last_name, score FROM person ORDER BY id DESC LIMIT %s;", (n,))
+        rows = cursor.fetchall()
+        print(f"Selected 최근 3개 데이터: {rows}")
+        return rows
+    except Exception as e:
+        print(f"Failed to select all data: {e}")
+        db.rollback()  # 예외 발생 시 롤백
+        return None
+
+def load_data():
+    db, cursor = connect_db()
+    if not db or not cursor:
+        print("Load_data 실패-db 연결x")
+        return None
+
+    try:
+        # 데이터 삽입
+        insert(db, cursor, 'lionel', 'messi', 97)
+        insert(db, cursor, 'jisung', 'park', 99)
+        insert(db, cursor, 'heungmin', 'son', 102)
+
+        # 모든 데이터 조회
+        all_data = select_n(db, cursor, 3) #최근 3개 데이터 조회
+        return all_data
+    finally:
+        close_db(db, cursor)
